32933276a87ea00227d12523126ebf977dba5cef
ktschroeder <ktschroeder@wisc.edu>
2022-10-10 00:05:20 -0500

    Create map_razor.py
    Complete tool that, given a Songs directory such as the normal one in osu, removes all files but taiko .osu files and their associated map audio files. Also verifies each map directory has exactly one audio file used across all maps in that directory.

---
+++ dataset_tools/map_razor.py
@@ -0,0 +1,60 @@
+import glob, os
+
+# returns audio filename from given .osu file
+def getAudioFilename(file):
+    f = open(file, 'rt')
+    for line in f:
+        try:
+            if line.index("AudioFilename: ") == 0:
+                return line.split(' ')[1].strip()
+        except:
+            continue
+    raise Exception(".osu file with no audio filename")
+
+# returns whether given map is a taiko map
+def isTaiko(file):
+    f = open(file, 'rt')
+    for line in f:
+        try:
+            if line.index("Mode: ") == 0:
+                isTaiko = line.split(' ')[1].strip() == '1'  # mode 1 corresponds to taiko mode
+                return isTaiko
+        except:
+            continue
+    raise Exception(".osu file with no mode")
+
+def delete(file):
+    try:
+        os.remove(file)
+        return 1
+    except:
+        print(f"Failed to delete file: {file}")
+        return 0
+
+
+mscFilesDeleted = 0  # miscellaneous files (anything but .osu) deleted counter
+osuFilesDeleted = 0  # .osu files deleted counter
+print("Provide address to collective Songs folder to simplify. Everything will be deleted except taiko .osu files and necessary audio files (and directories).")
+dirInput = input("")
+for root, dirs, files in os.walk(dirInput):
+    for dir in dirs:
+        os.chdir(os.path.join(root, dir))
+
+        audioFilename = ""
+        for file in glob.glob("*.osu"):
+            newAudioFilename = getAudioFilename(file)
+            if newAudioFilename != audioFilename:
+                assert not audioFilename, f"two different audio filenames in one map folder: {dir}"
+            audioFilename = newAudioFilename
+
+        assert audioFilename, f"no audio filename found among all osu files: {dir}"
+
+        for file in glob.glob("*"):
+            if file != audioFilename and file not in glob.glob("*.osu"):
+                mscFilesDeleted += delete(file)
+                
+        for file in glob.glob("*.osu"):
+            if not isTaiko(file):
+                osuFilesDeleted += delete(file)
+
+print(f"Deleted {osuFilesDeleted} non-taiko .osu files and {mscFilesDeleted} miscellaneous files.")
