9908082bb6c5d64ecacaade5fc4231f9a60f4c0d
ktschroeder <ktschroeder@wisc.edu>
2022-10-13 05:15:13 -0500

    Create audio_to_feats.py, implement more controllers, dataset management

--- dataset_tools/dataset_management.py
+++ dataset_tools/dataset_management.py
@@ -1,5 +1,9 @@
+import sys
+sys.path.append("C:/Users/Admin/Documents/GitHub/taiko_project")   # TODO cleaner way to do this?
+
 import os
 import map_to_json_converter
+from feature_extraction import audio_to_feats as atf
 
 def makeJsons():
     made = 0
@@ -11,4 +15,16 @@ def makeJsons():
                 made += map_to_json_converter.mapToJson(os.path.join(mainDir, songFolder, item))
     print(f"made {made} JSONs")
 
-makeJsons()
+#makeJsons()
+
+def makeMels():
+    made = 0
+    mainDir = "C:/Users/Admin/Documents/small_taiko_dataset"  # TODO temporary
+    for songFolder in os.listdir(mainDir):
+        for item in os.listdir(os.path.join(mainDir, songFolder)):
+            ext = os.path.splitext(item)[-1].lower()
+            if ext == ".wav":
+                made += atf.getFeats(os.path.join(mainDir, songFolder, item), songFolder)
+    print(f"made Mels for {made} song folders")
+
+makeMels()
--- onset_model_controller.py
+++ onset_model_controller.py
@@ -1,1 +1,17 @@
+import os
+import feature_extraction.map_json_to_feats as jtf
+
+def getMapFeats():
+    mainDir = "./data/json"
+    mapFeats = []
+    for item in os.listdir(mainDir):
+        mapFeats.append(jtf.jsonToFeats(item))
+
+def getSongFeats():
+    
+
+
+
 def basicModel():
+    mapFeats = getMapFeats()
+    songFeats = getSongFeats()
