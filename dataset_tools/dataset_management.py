1e156af95530203cc60b7bd97c4e332b2132d913
ktschroeder <ktschroeder@wisc.edu>
2022-11-26 02:07:44 -0600

    More cleaning

--- dataset_tools/dataset_management.py
+++ dataset_tools/dataset_management.py
@@ -9,33 +9,6 @@ from dataset_tools import audio_converter as ac
 
 from misc_tools import slugify
 
-# marked for deletion TODO
-# def makeJsons():
-#     made = 0
-#     mainDir = "C:/Users/Admin/Documents/medium_taiko_dataset"  # TODO temporary
-#     for songFolder in os.listdir(mainDir):
-#         for item in os.listdir(os.path.join(mainDir, songFolder)):
-#             ext = os.path.splitext(item)[-1].lower()
-#             if ext == ".osu":
-#                 made += map_to_json_converter.mapToJson(os.path.join(mainDir, songFolder, item))
-#     print(f"made {made} JSONs")
-
-#makeJsons()
-
-# marked for deletion TODO
-# def makeMels():  # want to send title and mapper
-#     jsons = makeJsons
-#     made = 0
-#     mainDir = "C:/Users/Admin/Documents/medium_taiko_dataset"  # TODO temporary
-#     for songFolder in os.listdir(mainDir):
-#         for item in os.listdir(os.path.join(mainDir, songFolder)):
-#             ext = os.path.splitext(item)[-1].lower()
-#             if ext == ".wav":
-#                 made += atf.getFeats(os.path.join(mainDir, songFolder, item), songFolder, jsons)
-#     print(f"made Mels for {made} song folders")
-
-#makeMels()
-
 def jointlyMakeJsonsAndMels():
     mainDir = "C:/Users/Admin/Documents/small_taiko_dataset"  # TODO temporary
     tempWavDir = "data/temp/temp.wav"
