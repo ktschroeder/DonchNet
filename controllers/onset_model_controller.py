47c7b33266851b6d359db837e07d020d723927e0
ktschroeder <ktschroeder@wisc.edu>
2022-11-26 02:07:07 -0600

    Clean up old commented-out code

--- controllers/onset_model_controller.py
+++ controllers/onset_model_controller.py
@@ -230,43 +230,10 @@ def prepareFeatsForModel(mapFeats, songFeats, mapCount):
 
     return pMapFeats, pSongFeats, starRatings
 
-# def createModel(mapFeats, songFeats):
-#     # model = keras.Sequential()
-#     # # Add an Embedding layer expecting input vocab of size 1000, and
-#     # # output embedding dimension of size 64.
-#     # model.add(layers.Embedding(input_dim=1000, output_dim=64))
-
-#     # # Add a LSTM layer with 128 internal units.
-#     # model.add(layers.LSTM(128))
-
-#     # # Add a Dense layer with 10 units.
-#     # model.add(layers.Dense(10))
-
-#     # model.summary()
-#     model = keras.Sequential()
-#     model.add(layers.Embedding(input_dim=1000, output_dim=64))
-
-#     # The output of GRU will be a 3D tensor of shape (batch_size, timesteps, 256)
-#     model.add(layers.GRU(256, return_sequences=True))
-
-#     # The output of SimpleRNN will be a 2D tensor of shape (batch_size, 128)
-#     model.add(layers.SimpleRNN(128))
-
-#     model.add(layers.Dense(10))
-
-#     model.summary()
-
-#     model.compile()  # TODO ensure my GPU is being used
-#     print("compiled model")
-
 def createConvLSTM():
     mapFeats, songFeats, count = jointlyGetMapAndSongFeats()
     print(count, "maps total")
     y, x, starRatings = prepareFeatsForModel(mapFeats, songFeats, count)
-    #nn = models.Sequential()
-    # nn.add(layers.Conv3D(?, activation = 'relu', input_dim=?)) # 
-    #nn.add(layers.Conv)
-    # print(np.shape(mapFeats[0][0]))
 
     # FULL CNN MODEL
     # convolutional layer with 10 filter kernels that are 7-wide in time and 3-wide in frequency. ReLU.
@@ -296,11 +263,6 @@ def createConvLSTM():
     # padding_value = -999
     # seq_length_cap = 30000  # 30000 frames = 300 seconds = 5 minutes
 
-    # xpad = np.full((17, seq_length_cap, 3), fill_value=padding_value)
-    # for s, x in enumerate(songFeats):
-    #     seq_len = x.shape[0]
-    #     xpad[s, 0:seq_len, :] = x
-    # print(xpad)
 
     print(x.shape, y.shape, starRatings.shape)  # currently (17, 24000, 15, 40) (17, 24000) (17,)
     
@@ -316,33 +278,9 @@ def createConvLSTM():
         stars[i] = np.full((max_sequence_length, 1), starRatings[i])
     starRatings = stars
 
-    # x = reshape(x, (len(x), 100, len(x[0][0]), 1,1))  # 17, 100, 40, 1
-    # y = y.reshape((len(y), 100 ,1,1))   
-    # x = x.astype(float32)
-    
-    # x = x.reshape((len(x), len(x[0]), 1, len(x[0][0]), len(x[0][0][0])))  # 17, 24000, 1, 15, 40
-    # print(x.shape, y.shape)
-
-
-    # Create Sequential Model ###########################################
     clear_session()
-    # model = Sequential()
-    # model.add(TimeDistributed(Conv2D(10, (7,3),activation='relu', padding='same',input_shape=(25,40,1,1),data_format='channels_first'))) # shape is 12+1+12 frames x 40 frequency bands x 1 row x 1 col
-    # model.add(TimeDistributed(MaxPool2D(pool_size=(1,3), padding='same')))
-    # model.add(TimeDistributed(Conv2D(20, (3,3),activation='relu', padding='same')))
-    # model.add(TimeDistributed(MaxPool2D(pool_size=(1,3), padding='same')))
-    # model.add(TimeDistributed(Flatten())) # see above notes, does this overly flatten temporal?
-    # model.add(LSTM(hidden_units, return_sequences=True))
-    # model.add(Dropout(0.5, noise_shape=(None,1,hidden_units)))  # is this shape correct?
-    # model.add(LSTM(hidden_units, return_sequences=True))  # TODO do we want return_sequences again, really?
-    # model.add(Dropout(0.5, noise_shape=(None,1,hidden_units))) 
-    # model.add(Dense(256, activation='relu'))
-    # model.add(Dropout(0.5)) 
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dropout(0.5)) 
 
     input = tf.keras.Input(shape=(max_sequence_length,15,40,1))
-
 
     # base_maps = tf.keras.layers.Lambda(context)(input)
     base_maps = TimeDistributed(Conv2D(10, (7,3),activation='relu', padding='same',data_format='channels_last'))(input) # TODO is 25 being scanned corectly?
@@ -391,34 +329,6 @@ def createConvLSTM():
     history = ga_model.fit([x, starRatings], y, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=0.2, )
     print(ga_model.summary())
 
-    
-    
-    
-    # opt = keras.optimizers.SGD(momentum=0.01, nesterov=True, learning_rate=learning_rate)
-    # # opt = AccumOptimizer(opt, 9) # number is accumulative steps, TODO
-
-    # opt = runai.ga.keras.optimizers.Optimizer(opt, steps=9)
-
-    # # model.compile(loss='mse', optimizer=opt)
-    # # model.fit(x_train, y_train, epochs=10, batch_size=10)
-
-
-    # model.compile(optimizer=opt, loss='binary_crossentropy')#, loss='binary_crossentropy'))# loss=error_to_signal, metrics=[error_to_signal]) previously used Adam
-    
-
-    # test_size = 0.2
-    # epochs = 4
-    # batch_size = 10  # 43 works with the defragmentation option enabled. This is a very small improvement.
-    # #35 works, 42 is OOM. No matter what number, dedicated GPU memory usage goes near max.
-    # #25 # ? decreasing this from total map count makes more parts per epoch. (frequently weights are updated)
-    # # batch size of 1 runs okay. Recently larger numbers crash (exceeding mem?). 5 throws CUDNN_STATUS_NOT_SUPPORTED, 50 throws plain mem error (OOM)
-
-    # print(x.shape, y.shape)
-    # print("begin fit")
-    # history = model.fit(x, y, batch_size=batch_size, epochs=epochs, verbose=1, validation_split=test_size)
-    # # model.build((17,1,30000,40,1))
-    # print(model.summary())
-    # print(history)
 
     ga_model.save("models/onset")
     # TODO save training history to be viewed later
@@ -427,61 +337,7 @@ def createConvLSTM():
         pickle.dump(history, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-    # indices = tf.range(start=0, limit=tf.shape(songFeats)[0], dtype=tf.int32)
-    # shuffled_indices = tf.random.shuffle(indices)
-
-    # X_random = tf.gather(songFeats, shuffled_indices)
-    # Y_random = tf.gather(mapFeats, shuffled_indices)
-    # songFeats = tf.convert_to_tensor(songFeats, dtype=tf.int32)
-    # x = songFeats.reshape(len(songFeats),1)
-
-    # mapFeats = np.asarray(mapFeats).astype('int32')
-    # y = mapFeats.reshape(len(mapFeats), 1)
-
-    
-    # shuffled_indices = np.random.permutation(len(songFeats)) 
-    # X_random = tf.gather(songFeats, shuffled_indices)
-    # y_random = tf.gather(mapFeats, shuffled_indices)
-
-    # mapFeats = tf.ragged.constant(mapFeats)
-    # print("begin fit")
-    # model.fit(x, y, epochs=epochs, batch_size=batch_size, validation_split=test_size, verbose=1)
-    
-    
-    # model.fit(np.asarray(songFeats).astype('float32'), np.asarray(mapFeats).astype('float32'), epochs=epochs, batch_size=batch_size, validation_split=test_size)
-    #model.save('test_model.h5')
-
     print("got to end")
-
-# def basicModel():
-    # mapFeats = getMapFeats()
-    # songFeats = getSongFeats()  # array of [mels pkl title, mels pkl data] pairs, one per song folder
-    # mapFeats, songFeats, totalMaps = jointlyGetMapAndSongFeats()
-    # mapFeats, songFeats = prepareFeatsForModel(mapFeats, songFeats, totalMaps)
-
-    # print(mapFeats.shape)
-    # print(songFeats.shape)
-    # mapFeats = sorted(mapFeats, key=lambda x: x[0])  # sort by id (makeshift title: song-mapper-diff)
-    # songFeats = sorted(songFeats, key=lambda x: x[0])  # similar to above
-    # no need to do above sorting with new implementation, they already line up.
-    # sloppy but at this point we expect the IDs to basically line up. But notably there are usually more mapFeats than songFeats: Some maps share a song.
-    # print(mapFeats[8][0][0])
-    # print(songFeats[8][0][0])
-
-    # for i in range(14):
-    #     print(len(mapFeats[i]), len(songFeats[i]))
-    # print(songFeats[0][0][1].shape)
-    # createConvLSTM()
-
-    # createModel(mapFeats, songFeats)
-    # print(songFeats[0][1][82])
-    # print(mapFeats.shape)
-    # print()
-    # print(songFeats[0][1].shape)
-
-# basicModel()
-
-
 
 
 createConvLSTM()
